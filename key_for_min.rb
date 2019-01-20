45a0b4e3d72f68da1af1c60850e30cbd9367dfc6
yong liang <yongliang24@gmail.com>
2019-01-20 23:44:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,19 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+ x=0
   if name_hash == nil
     return nil
 
   else
     name_hash.collect do |name, age|
-      
+
+      if age<x
+        x=age
+      end
+      x=age
+
+
 
     end
   end

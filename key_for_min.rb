d40d8b07a9afe368c05bb91ae8b2cf7b107e21d4
yong liang <yongliang24@gmail.com>
2019-01-20 23:46:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,19 +3,18 @@
 
 def key_for_min_value(name_hash)
  x=0
+ y=0
   if name_hash == nil
     return nil
 
   else
-    name_hash.collect do |name, age|
+    name_hash.each do |name, age|
 
       if age<x
-        x=age
+        y=age
       end
       x=age
-
-
-
     end
+    return y
   end
 end
